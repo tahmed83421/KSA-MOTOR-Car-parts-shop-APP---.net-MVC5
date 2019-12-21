24b2907c05db4f160cd1185576f253c9bf571a5f
tahmed83421 <55805582+tahmed83421@users.noreply.github.com>
2019-12-21 00:58:37 -0800

    Invoive class is made

--- BusinessLayer/Invoice.cs
+++ BusinessLayer/Invoice.cs
@@ -8,6 +8,23 @@ namespace BusinessLayer
 {
    public  class Invoice
     {
+        public int Invoice_ID { get; set; }
+        public int Customer_ID { get; set; }
+        public string To_Name { get; set; }
+        public string To_Phone { get; set; }
+        public string Ship_CompanyName { get; set; }
+        public string Ship_Adress { get; set; }
+        public string Ship_City { get; set; }
+        public string Ship_Phone { get; set; }
+        public int Vehicle_ID { get; set; }
+        public string Delivery_Date { get; set; }
+        public string Payment_Terms { get; set; }
+        public List<PartsInventory> parts { get; set; }
+        public int Weight { get; set; }
+        public int Total_Price { get; set; }
+
+
+
 
 
 
