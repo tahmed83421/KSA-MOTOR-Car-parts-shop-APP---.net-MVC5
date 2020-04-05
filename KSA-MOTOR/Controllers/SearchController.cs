58a1fdcc9f693445ee9394f5e880afa2f912cfab
Al Masum Fahim <masum_fahim@hotmail.com>
2020-04-05 00:05:18 -0700

    routing to search page implemented

--- KSA-MOTOR/Controllers/SearchController.cs
+++ KSA-MOTOR/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,18 @@ namespace KSA_MOTOR.Controllers
     public class SearchController : Controller
     {
         // GET: Search
-        public ActionResult Index(string s)
+        public ActionResult Index(int? NID, int? id, int? YID)
         {
+            Inventory inventory = new Inventory();
+            List<PartsInventory> parts;
+            if (NID == null || id == null || YID == null)
+            {
+                parts = inventory.Parts.ToList();
+            }
+            else
+            {
+                parts = inventory.Parts.Where(x => x.VModelId == NID).ToList();
+            }
             return View();
         }
     }
